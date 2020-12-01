e44b690efaed9789950b90c13bf31c0d935555ec
Joshua Johanning <jjohanning@10thmagnitude.com>
2020-12-01 12:18:06 -0600

    fixing again

--- app/main.tf
+++ app/main.tf
@@ -91,20 +91,6 @@ resource "azurerm_key_vault_access_policy" "service_principal" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "service_principal" {
-  key_vault_id = azurerm_key_vault.kv.id
-
-  tenant_id = var.key_vault_tenant_id
-  object_id = var.key_vault_spn_object_id
-
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-    "Delete"
-  ]
-}
-
 resource "azurerm_key_vault_access_policy" "my" {
   key_vault_id = azurerm_key_vault.kv.id
 
